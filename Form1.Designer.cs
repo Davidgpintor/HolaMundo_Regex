a9b4e22671a2d9ea0c512fd2c8b1479d23fe820a
ING. David Glez <ING. David Glez@DESKTOP-16LEFVB>
2021-11-24 20:42:07 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace HolaMundo_Regex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIngresoPass = new System.Windows.Forms.TextBox();
+            this.txtComparaPass = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa Pass:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresa nuevamente tu Pass.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtIngresoPass
+            // 
+            this.txtIngresoPass.Location = new System.Drawing.Point(220, 111);
+            this.txtIngresoPass.Name = "txtIngresoPass";
+            this.txtIngresoPass.Size = new System.Drawing.Size(132, 20);
+            this.txtIngresoPass.TabIndex = 2;
+            this.txtIngresoPass.TextChanged += new System.EventHandler(this.txtIngresoPass_TextChanged);
+            this.txtIngresoPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIngresoPass_KeyPress);
+            // 
+            // txtComparaPass
+            // 
+            this.txtComparaPass.Location = new System.Drawing.Point(220, 154);
+            this.txtComparaPass.Name = "txtComparaPass";
+            this.txtComparaPass.Size = new System.Drawing.Size(132, 20);
+            this.txtComparaPass.TabIndex = 3;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(258, 201);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(94, 38);
+            this.btnValidar.TabIndex = 4;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 268);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtComparaPass);
+            this.Controls.Add(this.txtIngresoPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIngresoPass;
+        private System.Windows.Forms.TextBox txtComparaPass;
+        private System.Windows.Forms.Button btnValidar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace HolaMundo_Regex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIngresoPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIngresoPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+           
+
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string primertxt = txtIngresoPass.Text;
+            string segundotxt = txtComparaPass.Text;
+
+            Regex regex = new Regex(@"^[a-zA-Z0-9´\\\/<>=\@#$%&()=¿?´¨¡!.;_+:-]+$");
+            
+            if (regex.IsMatch(primertxt) && primertxt == segundotxt)
+
+                MessageBox.Show("La Contraseña a sido Validada");
+
+            else
+                MessageBox.Show("Contraseña Invalida");
+        
+        }
+    }
+}
